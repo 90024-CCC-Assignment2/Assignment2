8b9a2230186081333e1d69dfb1cdf15576916e75
liushuk <81139622+liushuk@users.noreply.github.com>
2022-05-15 15:24:10 +1000

    Add files via upload

---
+++ Ansible deployment/run-nectar.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# COMP90024 Cluster and Cloud Computing Assignment2 - Team 42 
+# 
+# Authors: 
+#
+#  - Haoyu Gao (Student ID: 1141213)
+#  - Runqi Zhao (Student ID: 1159719)
+#  - Shukai Liu (Student ID: 1233459)
+#  - Siyu Liu (Student ID：1094521)
+#  - Wenjun Wang (Student ID: 1249890)
+#
+# Location: Melbourne
+#
+
+. ./unimelb-comp90024-2022-grp-42-openrc.sh; ansible-playbook -i hosts nectar.yaml
